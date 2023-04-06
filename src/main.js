b36c6abca62f5a4961f6b2a0cee150e3fc3f6287
mike <migowitch.work@gmail.com>
2023-04-06 22:51:25 +0300

    added vue-router, created login page

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import * as VueRouter from 'vue-router'
+import LoginPage from "@/pages/LoginPage.vue";
+import App from "@/App.vue";
 
-createApp(App).mount('#app')
+const routes = [
+    { path: '/login', component: LoginPage }
+]
+
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHistory(),
+    routes
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
