35a7414646178ab78bdb3d22482f0bb98f7314c5
mike <migowitch.work@gmail.com>
2023-04-06 23:41:30 +0300

    added style for input and login page

--- src/App.vue
+++ src/App.vue
@@ -8,3 +8,7 @@ export default {
   name: 'App',
 }
 </script>
+
+<style>
+@import "assets/css/main.css";
+</style>
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,5 +1,14 @@
 <template>
-  <h1>VueSuka</h1>
+    <div class="wrapper">
+        <div class="container">
+            <h3 class="login-header">Вход в систему Events</h3>
+            <form action="">
+                <input type="email" placeholder="Email">
+                <input type="password" placeholder="Пароль">
+                <button type="submit" class="btn-submit">Войти</button>
+            </form>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -8,6 +17,6 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style>
+@import "../assets/css/login.css";
 </style>
